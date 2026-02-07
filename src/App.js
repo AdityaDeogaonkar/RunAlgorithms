7b19f6d0227e0c6bf7213679611a72a54d3c9625
D_Aditya <aditya.deogaonkar@tripstack.com>
2026-02-07 17:04:57 +0530

    Integrate Google Analytics (G-ND7ZLP9KYK) for traffic tracking

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import ReactGA from 'react-ga4';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import TopicDetail from './pages/TopicDetail';
@@ -8,10 +9,25 @@ import POTDList from './pages/potd/POTDList';
 import POTDDetail from './pages/potd/POTDDetail';
 import './App.css';
 
+// Initialize GA4 with your Measurement ID
+ReactGA.initialize('G-ND7ZLP9KYK');
+
+// Component to track page views
+const PageViewTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
+
+  return null;
+};
+
 function App() {
   return (
     <Router>
       <div className="App d-flex flex-column min-vh-100">
+        <PageViewTracker />
         <Navigation />
         <div className="flex-grow-1">
           <Routes>
